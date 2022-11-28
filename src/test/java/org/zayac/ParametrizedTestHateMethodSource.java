a64da8541e457cb237bf3fbf9584c09b0cdf06ea
Valery <qaivlev@yandex.ru>
2022-11-28 20:00:20 +0300

    delete import

--- src/test/java/org/zayac/ParametrizedTestHateMethodSource.java
+++ src/test/java/org/zayac/ParametrizedTestHateMethodSource.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
